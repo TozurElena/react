ec77b2ad09bf23eaaeee5f205fa27680ed181965
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-02 21:12:01 +0200

    commencer Hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React from 'react';
+// import {useState} from 'react';
 
+import './App.css';
+// import Hello from './hello';
 
-function App() {
-  let likes = 0;
-  return (
-    <div className="App">
-      <h1>{likes}</h1> 
-      <button onClick={() => likes +=1}>Increment</button>
-      <button onClick={() => likes -=1}>Decrement</button>
-    </div>
-  );
+// function App() {
+  // const [state, setState] = useState(initialState);
+  // console.log(state);
+  // const [likes, setLikes] = useState(initialState);
+  // function increment() {
+  //   setLikes(value.likes + 1)
+  // }
+  // function decrement() {
+  //   setLikes(value: likes - 1)
+  // }
+//     return (
+//     <div className="App">
+      
+//       {/* <h1>{likes}</h1>  */}
+//       <Hello/>
+//       {/* <button onClick={increment}>Increment</button>
+//       <button onClick={decrement}>Decrement</button> */}
+//     </div>
+//   );
+// }
+// export default App;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={() => this.setState({count: this.state.count = 1})}>
+          Click me
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
