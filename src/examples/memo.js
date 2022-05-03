c6f82667f7dcec45cd58daaa27f84dd12fc3e9b2
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-05-03 17:14:56 +0200

    add useMemo

--- src/examples/memo.js
+++ src/examples/memo.js
@@ -1,15 +1,28 @@
 import React, {useState, useMemo}  from 'react';
 
+function complexCompute(num) {
+  let i = 0
+  while (i < 1000000000) i++;
+  return num * 2;
+}
 
 function App () {
   // initialize the useStates
   const [number, setNumber] = useState(42);
   const [colored, setColored] = useState(false);
   
+  const styles = useMemo(() => ({
+    color: colored ? 'darkred' : 'black'
+  }), [colored]);
     
+  const computed = useMemo(() => {
+    return complexCompute(number);
+  }, [number]) 
+
+  
   return (
     <div>
-      <h1 style={}>Сomputed property: {}</h1>
+      <h1 style={styles}>Сomputed property: {computed}</h1>
       <button className={'btn btn-success'} onClick={() => setNumber(prev => prev + 1)}>Add</button>
       <button className={'btn btn-danger'} onClick={() => setNumber(prev => prev - 1)}>Clean</button>
       <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Modifier</button>
